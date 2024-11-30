067628bc55fc079dd470b0717d3e0ac27fd2ed69
Admin <ashok.marketyard@gmail.com>
2024-12-01 00:15:02 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println(new Date());
         System.out.println("hello");
         System.out.println("added one more file");
+
+        Student student=new Student();
+        student.sayHello();
     }
 }
